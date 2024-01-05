13dd975201e9ed3457fb486b131e426fb0926a78
Martin Renteria <mrrenteria0159@gmail.com>
2024-01-04 21:08:48 -0700

    setting up delete routes

--- controllers/api/venueRoutes.js
+++ controllers/api/venueRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { Venues } = require('../../models')
+const withAuth = require('../../utils/auth')
 
 router.get('/', async (req, res) => {
     try {
@@ -14,5 +15,24 @@ router.get('/', async (req, res) => {
         res.status(400).json(err)
     }
 })
+
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const venueData = await Venues.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+        if(!venueData) {
+            res.status(404).json({ message: 'No gig found with this id!' });
+            return;
+        }
+        res.status(200).json(venueData);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+});
 
 module.exports = router
