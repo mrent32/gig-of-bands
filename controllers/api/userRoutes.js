c8564eab381d47e978f0777376e7e9f6ca0e1f51
Martin Renteria <mrrenteria0159@gmail.com>
2024-01-08 19:26:19 -0700

    login routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,11 +1,51 @@
 const router = require('express').Router()
-const { Bands, Venues } = require('../../models')
+const { Bands, Venues } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/login', withAuth, async (req, res) => {
+  try {
+    console.log(req.body)
+    let userData 
+    if(req.body.dropbox == 'band') {
+       userData = await Bands.findOne({ where: { username: req.body.username } });
+    } else if(req.body.dropbox == 'venue') {
+       userData = await Venues.findOne({ where: { username: req.body.username } });
+    }
+    
+  
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+      res.render('profile')
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 
 router.post('/login', async (req, res) => {
   try {
-    console.log(req.body)
-    const userData = await Bands.findOne({ where: { username: req.body.email } });
-    console.log(userData)
+    const userData = await Bands.findOne({ where: { username: req.body.name } });
 
     if (!userData) {
       res
@@ -29,10 +69,9 @@ router.post('/login', async (req, res) => {
       
       res.json({ user: userData, message: 'You are now logged in!' });
     });
-    
+
   } catch (err) {
     res.status(400).json(err);
-    console.log(err)
   }
 });
 
