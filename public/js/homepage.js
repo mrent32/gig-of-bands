1bebaf51808347841c78e2551d9e6d4f10ddf000
Martin Renteria <mrrenteria0159@gmail.com>
2024-01-10 19:44:50 -0700

    added calendar to homepage

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,3 +1,4 @@
+// 
 const viewPage = (event) => {
     event.preventDefault()
 
@@ -9,3 +10,78 @@ const viewPage = (event) => {
 [...document.querySelectorAll('.container')]
     .forEach(container => container
     .addEventListener('click', viewPage))
+
+    const cal = document.getElementById('calendar2')
+
+let calendar = new FullCalendar.Calendar(cal, {
+  initialView: 'dayGridMonth',
+  selectable: true,
+  headerToolbar: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,listWeek'
+  },
+  events: [
+    {
+      title: 'Jon Bovi live at Starlight Pavilion!',
+      start: '2024-01-11'
+    },
+    {
+        title: `Crimson Tide live at 
+        Whispering Woods`,
+        start: '2024-01-12'
+    },
+    {
+        title: `The Stolling Rones live at 
+        Harbor Lights Pavilion!`,
+        start: '2024-01-13'
+    },
+    {
+        title: `Pysychadelica live at
+         Whirlwind Hall!`,
+        start: '2024-01-14'
+    },
+    {
+        title: `Epic Explorers live 
+        at Sapphire Lounge!`,
+        start: '2024-01-15'
+    },
+    {
+        title: `Galactic Groovers live
+         at Aurora Amphitheater`,
+        start: '2024-01-16'
+    },
+    {
+        title: `Radiant Rebels live at 
+        Majestic Ballrom!`,
+        start: '2024-01-17'
+    },
+    {
+        title: `Lunar Lullabies live
+         at Harmony Haven!`,
+        start: '2024-01-19'
+    },
+    {
+        title: `Sonic Boom live 
+        at Crystal Palace!`,
+        start: '2024-01-18'
+    },
+    {
+        title: `Eclipse Enigma live
+         at Rustic Retreat!`,
+        start: '2024-01-20'
+    },
+    {
+        title: `Mystic Melodies live at 
+        Vivid Vista Lounge!`,
+        start: '2024-01-21'
+    },
+    {
+        title: `Electric Echoes live 
+        at Ethereal Oasis!`,
+        start: '2024-01-22'
+    },
+
+  ]
+})
+calendar.render()
